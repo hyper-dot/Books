ea64b1812c8c385393262f14f633301e4d9fd7fc
Roshan Paudel <rozanpoudel@gmail.com>
2024-08-10 10:12:56 +0545

    🐞 FIX: hero image preload

--- src/components/hero/HeroImage.tsx
+++ src/components/hero/HeroImage.tsx
@@ -1,25 +1,31 @@
 "use client";
-import { motion, useTransform, useViewportScroll } from "framer-motion";
+import { motion, useTransform, useScroll } from "framer-motion";
+import Head from "next/head";
 
 const HeroImage = () => {
-  const { scrollYProgress } = useViewportScroll();
+  const { scrollYProgress } = useScroll();
   const scale = useTransform(scrollYProgress, [0, 1], [1, 1.5]);
 
   return (
-    <div>
-      <motion.img
-        style={{ scale }}
-        src="/hero.png"
-        className="dark:hidden w-full md:w-[70%] mx-auto"
-        alt=""
-      />
-      <motion.img
-        style={{ scale }}
-        src="/hero-dark.png"
-        className="hidden dark:block w-full md:w-[70%]  mx-auto"
-        alt=""
-      />
-    </div>
+    <>
+      <Head>
+        <link rel="preload" href="/path/to/image.ext" as="image" />
+      </Head>
+      <div>
+        <motion.img
+          style={{ scale }}
+          src="/hero.png"
+          className="dark:hidden w-full md:w-[70%] mx-auto min-h-[500px]"
+          alt=""
+        />
+        <motion.img
+          style={{ scale }}
+          src="/hero-dark.png"
+          className="hidden dark:block w-full md:w-[70%]  mx-auto"
+          alt=""
+        />
+      </div>
+    </>
   );
 };
 
