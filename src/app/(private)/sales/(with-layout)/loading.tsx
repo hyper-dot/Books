c0ba5c507acd5aabe2e3ba868c9a930eadfbdf69
Roshan Paudel <rozanpoudel@gmail.com>
2024-10-14 20:03:36 +0545

    🐞 FIX: sales page loading

---
+++ src/app/(private)/sales/(with-layout)/loading.tsx
@@ -0,0 +1,8 @@
+import DataTableSkeleton from "@/components/skeletons/DataTableSkeleton";
+import React from "react";
+
+const loading = () => {
+  return <DataTableSkeleton />;
+};
+
+export default loading;
